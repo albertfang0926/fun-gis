ccf052f08728e8be98611a56e671b6ce9768479d
Albert <albertfang0926@qq.com>
2023-08-16 00:07:22 +0800

    [update]Set home view

--- src/components/CesiumViewer/index.vue
+++ src/components/CesiumViewer/index.vue
@@ -3,17 +3,40 @@
 </template>
 
 <script setup lang="ts">
-import { Viewer, ImageryLayer, TileMapServiceImageryProvider, buildModuleUrl } from "cesium"
+import {
+  Viewer,
+  Cartesian3,
+  ImageryLayer,
+  TileMapServiceImageryProvider,
+  buildModuleUrl
+} from "cesium"
 import { onMounted, ref } from "vue"
 import { useViewerStore } from "@/stores/cesiumViewer"
 const viewerRef = ref()
 onMounted(async () => {
   const viewer = new Viewer(viewerRef.value, {
-    baseLayer: new ImageryLayer(
-      await TileMapServiceImageryProvider.fromUrl(buildModuleUrl("Assets/Textures/NaturalEarthII")),
-      {}
-    )
+    animation: false,
+    homeButton: true,
+    geocoder: true,
+    baseLayerPicker: false,
+    timeline: false,
+    fullscreenButton: false,
+    infoBox: false,
+    sceneModePicker: true,
+    navigationInstructionsInitiallyVisible: false,
+    navigationHelpButton: false,
+    selectionIndicator: false
   })
+  ;(viewer.cesiumWidget.creditContainer as any).style.display = "none"
+  // viewer.scene.base
+  // const viewer = new Viewer(viewerRef.value, {
+  //   baseLayer: new ImageryLayer(
+  //     await TileMapServiceImageryProvider.fromUrl(buildModuleUrl("Assets/Textures/NaturalEarthII")),
+  //     {}
+  //   )
+  // })
+  const homeCoord: [number, number, number] = [117.43111, 32.100556, 1e6]
+  viewer.camera.setView({ destination: Cartesian3.fromDegrees(...homeCoord) })
   useViewerStore().setViewer(viewer)
 })
 </script>
