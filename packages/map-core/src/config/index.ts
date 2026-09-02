d74c462e737e19d7dce707cf67cdff43125ad5ac
albertfang <albertfang0926@qq.com>
2026-09-02 17:11:16 +0800

    chore: vscode 设置

--- packages/map-core/src/config/index.ts
+++ packages/map-core/src/config/index.ts
@@ -1,8 +1,8 @@
 // 配置类型已迁移至 types/index.d.ts（IMapConfig）
 // 此文件保留以避免破坏导入路径，重新导出相关类型
+export { DEFAULT_HOME_POSITION } from "../core/initMap/default-opts"
 export type {
-  IMapConfig,
   IImagerySourceConfig,
+  IMapConfig,
   ITerrainSourceConfig
 } from "../types/index"
-export { DEFAULT_HOME_POSITION } from "../core/initMap/default-opts"
